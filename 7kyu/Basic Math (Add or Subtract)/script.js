c99e1d7b914dc362075c68161a7d77aa84a6f1b6
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2020-01-07 22:04:13 +0300

    Will you make it? || 8kyu
    https://www.codewars.com/kata/will-you-make-it/javascript

--- 7kyu/Basic Math (Add or Subtract)/script.js
+++ 7kyu/Basic Math (Add or Subtract)/script.js
@@ -1,11 +1,52 @@
 function calculate(str) {
-    let res = [];
+    let arr = [];
 
-    for (let i = 0; i < str.length; i++) {
+    if (parseInt(str)) {
+        arr.push(parseInt(str));
+        str = str.split(``);
+        str.splice(0, String(arr[0]).length);
+        str = str.join(``);
 
 
     }
-    return String(res);
+
+    //console.log(`arr--->${arr}`);
+    //console.log(`str--->${str}`);
+
+
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[0] === `p`) {
+            str = str.split(``);
+            str.splice(0, 4);
+            str = str.join(``);
+            arr.push(parseInt(str));
+            str = str.split(``);
+            str.splice(0, String(arr[arr.length - 1]).length);
+            str = str.join(``);
+        }
+
+        if (str[0] === `m`) {
+            str = str.split(``);
+            str.splice(0, 5);
+            str = str.join(``);
+            arr.push(parseInt(str) * (-1));
+            str = str.split(``);
+            str.splice(0, String(arr[arr.length - 1]).length - 1);
+            str = str.join(``);
+        }
+
+        if (parseInt(str)) {
+
+        }
+
+        console.log(arr);
+        e2df
+        return arr.reduce((sum, item) => sum + item, 0)
+
+    }
+
+
 }
 
-console.log(calculate(`11minus2minus3minus4`));
+console.log(calculate(`3plus7plus7`));
---
+++ 8kyu/Will you make it/script.js
@@ -0,0 +1,7 @@
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    return mpg * fuelLeft >= distanceToPump;
+};
+
+/*
+    https://www.codewars.com/kata/will-you-make-it/javascript
+*/
