14391d9f5e95a07649875b1c414086fb09af8469
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2020-01-14 19:06:09 +0300

    Count the divisors of a number || 7kyu
    https://www.codewars.com/kata/542c0f198e077084c0000c2e/javascript

---
+++ 7kyu/Count the divisors of a number/script.js
@@ -0,0 +1,13 @@
+function getDivisorsCnt(n) {
+    let res = 0;
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) {
+            res++;
+        }
+    }
+    return res;
+}
+
+/*
+    https://www.codewars.com/kata/542c0f198e077084c0000c2e/javascript
+*/
