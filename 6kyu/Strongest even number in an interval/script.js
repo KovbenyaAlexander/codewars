5627aa0ee2310c5845d836f1eef69a4ae9f79f34
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2020-01-20 20:17:54 +0300

    minor fix

--- 6kyu/Strongest even number in an interval/script.js
+++ 6kyu/Strongest even number in an interval/script.js
@@ -1,6 +1,29 @@
 function strongestEven(n, m) {
+    if (n % 2 === 1) {
+        n++;
+    }
+
+    for (let i = n; i <= m; i += 2) {
+        if (Math.log2(i) % 1 === 0 && i * 2 > m) {
+            return i;
+        }
+    }
+
+
+
+
+
+
+
+
     let arr = [];
-    for (let i = n; i <= m; i++) {
+    if (n % 2 === 1) {
+        n++;
+    }
+    for (let i = n; i <= m; i += 2) {
+        if (Math.log2(i) % 1 === 0 && i * 2 > m) {
+            return i;
+        }
         arr.push(i);
     }
 
@@ -23,14 +46,80 @@ function strongestEven(n, m) {
     max = Math.max(...arrStrg);
     console.log(max);
 
-    return max;
-    let arrMax = [];
-    for (let i = 0; i < arrStrg.length; i++) {
-        if (arrStrg[i] === max) {
-            arrMax.push(i);
-        }
-    }
-    console.log(arrMax);
+    let ind = arrStrg.findIndex(item => item === max);
+    console.log(ind);
+    return arr[ind];
+
+
+
+
+
+
+
+
 }
 
-strongestEven(1, 15);
+
+
+
+
+
+console.log(strongestEven(1, 15));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+//for little numbers
+
+
+function strongestEven(n, m) {
+    let arr = [];
+    if (n % 2 === 1) {
+        n++;
+    }
+    for (let i = n; i <= m; i += 2) {
+        arr.push(i);
+    }
+
+    console.log(arr);
+    let arrStrg = [];
+
+
+    arr.forEach(item => {
+        let strong = 0;
+        while (item % 2 == 0) {
+            item = item / 2;
+            strong++;
+        }
+        arrStrg.push(strong);
+    });
+    console.log(arrStrg);
+
+    let max;
+
+    max = Math.max(...arrStrg);
+    console.log(max);
+
+    let ind = arrStrg.findIndex(item => item === max);
+    console.log(ind);
+    return arr[ind];
+}
+
+*/
