b3bd8422edee9e64fd62bf31a66742fca6b53320
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2020-02-18 19:50:08 +0300

    Pandemia 🌡️ || 7kyu
    https://www.codewars.com/kata/5e2596a9ad937f002e510435/train/javascript

---
+++ 7kyu/Pandemia 🌡️/script.js
@@ -0,0 +1,19 @@
+function infected(s) {
+    let total = s.split(``).filter(item => item != `X`).length
+    let infected = [];
+    s.split(`X`).forEach(item => {
+        if (item.split(``).findIndex(person => person == 1) != -1) {
+            infected.push(item);
+        }
+    });
+
+    /*
+        console.log(s);
+        console.log(`total--->${total}`); //+
+        console.log(`infected--->${infected.join(``).length}`); //+
+        console.log(infected);*/
+
+    return fvsdrwf100 * infected.join(``).length / total
+}
+
+console.log(infected(`X00X000000X10X0100`));
