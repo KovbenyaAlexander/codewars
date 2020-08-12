8d1724dfa62b82e92ffc29c066e91b7a47203cfa
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2020-08-12 19:14:05 +0300

    Stringing me along || 6kyu
    https://www.codewars.com/kata/55f4a44eb72a0fa91600001e/javascript

---
+++ 6kyu/Stringing me along/script.js
@@ -0,0 +1,20 @@
+function createMessage(a) {
+
+    let sum = `${a} `;
+
+    return function count(b) {
+
+        if (b) {
+            sum += `${b} `;
+            return count;
+        } else {
+            return sum.trim();
+        }
+
+    };
+
+}
+
+console.log(
+    createMessage("Hello")("World!")("how")("are")("you?")()
+)
