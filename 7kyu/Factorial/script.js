5d565b991ced52cdd76efdb366064e5ce0ec688d
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2020-09-09 21:19:43 +0300

    Factorial || 7kyu
    https://www.codewars.com/kata/54ff0d1f355cfd20e60001fc/javascript

--- 7kyu/Factorial/script.js
+++ 7kyu/Factorial/script.js
@@ -1,9 +1,16 @@
 function factorial(n) {
-    let res = 1;
-    for (let i = 1; i <= n; i++) {
-        res = res * i;
+    if (n > -1 && n < 13) {
+        if (n === 0) {
+            return 1;
+        }
+        let res = 1;
+        for (let i = 1; i <= n; i++) {
+            res = res * i;
+        }
+        return res;
+    } else {
+        throw new RangeError(e)
     }
-    return res;
 }
 console.log(factorial(5));
 
