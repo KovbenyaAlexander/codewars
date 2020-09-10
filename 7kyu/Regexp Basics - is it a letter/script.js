1c76af5ad330c9fd3c1b4c2cc738db97340f2479
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2020-09-10 20:24:14 +0300

    fix

--- 7kyu/Regexp Basics - is it a letter/script.js
+++ 7kyu/Regexp Basics - is it a letter/script.js
@@ -1,11 +1,13 @@
 String.prototype.isLetter = function () {
 
-
-    if (!this) {
+    if (!this || this.length != 1) {
         return false
     }
-    console.log(this.match(/[A-Za-z]{1}$/gm))
-    return this.match(/[A-Za-z]{1}$/gm);
+
+    return !!this.match(/[A-Za-z]{1}$/gm);
 }
+
+
+
 
 console.log(`h`.isLetter());
